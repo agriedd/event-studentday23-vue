78c51cb520e56b4f1347268c240f765b4a7756b2
Agri Edd. Oematan <dnr739528@gmail.com>
2023-04-30 20:29:42 +0800

    update overflow hiden

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -33,10 +33,10 @@ const options = ref(false)
 <template>
 	<div class="relative bg-neutral-950">
 		<div class="p-4 flex md:flex-row flex-col justify-center items-center w-full text-white 
-			min-h-screen absolute inset-y-0 inset-x-0 h-screen">
-			<img src="https://agriedd.github.io/studentday/assets/code.png" class="h-[80vh] w-auto rounded-md opacity-40" alt="">
+			min-h-screen absolute inset-y-0 inset-x-0 h-screen max-h-screen overflow-hidden">
+			<img src="https://agriedd.github.io/studentday/assets/code.png" class="h-[80vh] w-auto rounded-md opacity-20" alt="">
 			<div>
-				<img src="https://agriedd.github.io/studentday/assets/studentday selected-08.png" class="h-[80vh] w-auto blur-md" alt="">
+				<img src="https://agriedd.github.io/studentday/assets/studentday selected-08.png" class="h-[80vh] w-auto blur-2xl" alt="">
 			</div>
 		</div>
 		<div class="relative p-4 min-h-screen bg-slate-900 bg-opacity-70 
